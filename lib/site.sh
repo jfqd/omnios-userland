aab5f8b156c571f5686230758b6a47f1c358098f
jfqd <jfqd@blun.org>
2014-12-08 16:58:09 +0100

    fix pkg publisher

--- lib/site.sh
+++ lib/site.sh
@@ -1,6 +1,6 @@
 # Package server URL and publisher
-PKGPUBLISHER=r151006.qutic
-PKGSRVR=http://localhost:10001/
+PKGPUBLISHER=application
+PKGSRVR=http://pkg.qutic.net:10001/
 
 # Uncommenting this line will create a on-disk repo in
 # the top level of your checkout and publish there instead
