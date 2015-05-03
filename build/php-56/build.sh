14d77a1e1e616a5acbf150726ad54db7e20b4461
jfqd <jfqd@blun.org>
2015-05-03 13:34:38 +0200

    update version

--- build/php-56/build.sh
+++ build/php-56/build.sh
@@ -29,7 +29,7 @@
 
 # http://de1.php.net/get/php-5.6.6.tar.gz/from/this/mirror
 PROG=php
-VER=5.6.6
+VER=5.6.8
 PKG=runtime/php56
 SUMMARY="PHP Server 5.6"
 DESC="PHP is a widely-used general-purpose scripting language that is especially suited for Web development and can be embedded into HTML."
