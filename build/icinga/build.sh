b37d18ac52978d267c9beb20d7ff54bd6d32bc95
jfqd <jfqd@blun.org>
2015-08-21 09:51:31 +0200

    it is build, not make

--- build/icinga/build.sh
+++ build/icinga/build.sh
@@ -34,7 +34,7 @@ PKG=monitoring/icinga2
 SUMMARY="Open source monitoring system"
 DESC="Icinga 2 is an open source monitoring system which checks the availability of your network resources, notifies users of outages, and generates performance data for reporting."
 
-make() {
+build() {
   logmsg "Build icinga with cmake $TMPDIR/$BUILDDIR"
   pushd $TMPDIR/$BUILDDIR >/dev/null
   logcmd mkdir build && cd build
