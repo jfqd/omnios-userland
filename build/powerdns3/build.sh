8f988185ca6c781f5e2c9c2a6c174d7f2c970d08
jfqd <jfqd@blun.org>
2016-02-02 12:54:35 +0100

    update version to 3.4.7

--- build/powerdns3/build.sh
+++ build/powerdns3/build.sh
@@ -29,7 +29,7 @@
 
 # https://downloads.powerdns.com/releases/pdns-3.4.5.tar.bz2
 PROG=powerdns
-VER=3.4.6
+VER=3.4.7
 VERHUMAN=$VER
 PKG=service/network/dns/powerdns3
 SUMMARY="DNS Authoritative Server"
