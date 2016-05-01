fb4e2efec1689116e96808350dfdc6c7edf950a9
jfqd <jfqd@blun.org>
2016-05-01 13:22:56 +0200

    include the right automx

--- build/python27-automx/build.sh
+++ build/python27-automx/build.sh
@@ -21,28 +21,35 @@
 # CDDL HEADER END
 #
 #
-# Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Copyright 2016 OmniTI Computer Consulting, Inc.  All rights reserved.
 # Use is subject to license terms.
 #
 # Load support functions
 . ../../lib/functions.sh
 
-PROG=myapp      # App name
-VER=            # App version
-VERHUMAN=$VER   # Human-readable version
-#PVER=          # Branch (set in config.sh, override here if needed)
-PKG=            # Package name (e.g. library/foo)
-SUMMARY=""      # One-liner, must be filled in
-DESC=""         # Longer description, must be filled in
+PROG=automx
+VER=0.10.2
+VERHUMAN=$VER
+PKG=runtime/python27/automx
+DOWNLOADURL="https://github.com/sys4/automx/archive/v0.10.2.tar.gz"
+SUMMARY="mail client account configuration service"
+DESC="automx is a $SUMMARY ($VER)"
+
+BUILDARCH=32
+PYTHON=/usr/local/bin/python2.7
+
+DEPENDS_IPS="runtime/python27 python27/lxml python27/mysql python27/m2mcrypto"
+BUILD_DEPENDS_IPS="$DEPENDS_IPS"
+
+# Additionally we need the following libraries.
+# Later we will build a package for each.
+#
+# pip install python-dateutil ipaddr python-memcached python-sqlalchemy
 
 init
 download_source $PROG $PROG $VER
 patch_source
 prep_build
-build
-make_isa_stub
+python_build
 make_package
 clean_up
-
-# Vim hints
-# vim:ts=4:sw=4:et:
