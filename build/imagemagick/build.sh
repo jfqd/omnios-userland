b9df20cef9ba948329c51258dd429fbe3a1efb98
jfqd <jfqd@blun.org>
2016-05-09 12:28:20 +0200

    revert to version 6.9.0-7 (but with policymap in place), cause 6.9.1-4 had issues in production

--- build/imagemagick/build.sh
+++ build/imagemagick/build.sh
@@ -28,7 +28,7 @@
 . ../../lib/functions.sh
 
 PROG=ImageMagick
-VER=6.9.1-4
+VER=6.9.0-7
 VERHUMAN=$VER
 PKG=image/imagemagick
 SUMMARY="software suite to create, edit, compose, or convert bitmap images"
