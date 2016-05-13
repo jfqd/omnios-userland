2091597dda0b77a94366a21d03f73298711006c9
jfqd <jfqd@blun.org>
2016-05-13 09:31:02 +0200

    finally fixed with help (github.com/ImageMagick/ImageMagick/issues/191)

--- build/imagemagick/build.sh
+++ build/imagemagick/build.sh
@@ -28,7 +28,7 @@
 . ../../lib/functions.sh
 
 PROG=ImageMagick
-VER=6.9.0-7
+VER=6.9.4-1
 VERHUMAN=$VER
 PKG=image/imagemagick
 SUMMARY="software suite to create, edit, compose, or convert bitmap images"
@@ -38,22 +38,17 @@ BUILD_DEPENDS_IPS="library/freetype2 library/libjpeg library/libpng library/libt
 DEPENDS_IPS="library/freetype2 library/libjpeg library/libpng library/libtiff"
 
 CFLAGS="-I/usr/local/include"
-LDFLAGS32="$LDFLAGS32 -L/usr/local/lib -R/usr/local/lib"
-LDFLAGS64="$LDFLAGS64 -L/usr/local/lib/$ISAPART64 -R/usr/local/lib/$ISAPART64"
+LDFLAGS32="$LDFLAGS32 -L/usr/local/lib -R/usr/local/lib -lsocket -lnsl"
+LDFLAGS64="$LDFLAGS64 -L/usr/local/lib/$ISAPART64 -R/usr/local/lib/$ISAPART64 -lsocket -lnsl"
 
 CONFIGURE_OPTS="--disable-openmp --disable-static --without-x"
-
-fix_cve() {
-    logcmd cp $SRCDIR/files/policy.xml \
-        $DESTDIR/usr/local/etc/ImageMagick-6/policy.xml
-}
 
 init
 download_source imagemagick $PROG $VER
 patch_source
 prep_build
+
 build
-fix_cve
 make_isa_stub
 PROG=imagemagick
 VER=${VER//-/.}
