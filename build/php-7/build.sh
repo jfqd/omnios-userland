a1ac0ababa84e75c6a668d33d9ea4681119ca8f5
jfqd <jfqd@blun.org>
2016-11-26 16:39:11 +0100

    oh no: php7 do not compile

--- build/php-7/build.sh
+++ build/php-7/build.sh
@@ -29,7 +29,7 @@
 
 # http://de1.php.net/get/php-5.6.17.tar.gz/from/this/mirror
 PROG=php
-VER=7.0.5
+VER=7.0.13
 PKG=runtime/php7
 SUMMARY="PHP Server 7"
 DESC="PHP is a widely-used general-purpose scripting language that is especially suited for Web development and can be embedded into HTML."
@@ -60,24 +60,27 @@ BUILD_DEPENDS_IPS="compress/bzip2
 
 # Though not strictly needed since we override build(), still nice to set
 BUILDARCH=64
-PREFIX=$PREFIX/php56
+PREFIX=$PREFIX/php7
 reset_configure_opts
 
-#CFLAGS="-O2 -DZLIB_INTERNAL=1 -std=c99"
-CFLAGS="-O2 -DZLIB_INTERNAL=1 -std=gnu99"
-CPPFLAGS="-I/usr/local/include"
-LDFLAGS="-L/usr/local/lib -R/usr/local/lib \
-    -L$PREFIX/lib -R$PREFIX/lib"
+# CFLAGS="-O2 -DZLIB_INTERNAL=1 -std=gnu99"
 
-CPPFLAGS64="-I/usr/local/include/$ISAPART64 -I/usr/local/include/$ISAPART64/curl \
-    -I/usr/local/include -I /usr/local/include/mysql"
-LDFLAGS64="$LDFLAGS64 -L/usr/local/lib/$ISAPART64 -R/usr/local/lib/$ISAPART64 \
-    -L$PREFIX/lib -R$PREFIX/lib"
+#CPPFLAGS="-I/usr/local/include"
+
+#LDFLAGS="-L/usr/local/lib -R/usr/local/lib -L$PREFIX/lib -R$PREFIX/lib"
+
+#CPPFLAGS64="-I/usr/local/include/$ISAPART64 -I/usr/local/include/$ISAPART64/curl -I/usr/local/include -I /usr/local/include/mysql"
+
+#LDFLAGS64="$LDFLAGS64 -L/usr/local/lib/$ISAPART64 -R/usr/local/lib/$ISAPART64 -L$PREFIX/lib -R$PREFIX/lib"
 
 # https://www.mail-archive.com/php-install@lists.php.net/msg16137.html
-export PHP_MYSQLND_ENABLED=yes
+#export PHP_MYSQLND_ENABLED=yes
+#export PHP_LIBXML_SHARED="1"
 
-export EXTENSION_DIR=$PREFIX/lib/modules
+# LDFLAGS="$LDFLAGS -lsocket -lnsl -lcrypto -lmysqlclient -lm -lssl"
+
+#export EXTENSION_DIR=$PREFIX/lib/modules
+
 CONFIGURE_OPTS_32=""
 CONFIGURE_OPTS_64=""
 CONFIGURE_OPTS="
@@ -93,8 +96,6 @@ CONFIGURE_OPTS="
         --libexecdir=$PREFIX/libexec
         --datarootdir=$PREFIX/share
         --mandir=$PREFIX/man
-        --with-pear=$PREFIX/pear
-        --enable-dtrace
         --enable-fpm
         --enable-xml
         --enable-simplexml
@@ -102,19 +103,16 @@ CONFIGURE_OPTS="
         --with-zlib=shared
         --with-zlib-dir=/usr/local
         --with-sqlite3=shared
-        --enable-pdo=shared
-        --with-pdo-sqlite=shared
-        --with-mysql=shared,mysqlnd
-        --with-mysqli=shared,mysqlnd
-        --with-pdo-mysql=shared,mysqlnd
+        --without-mysqli
         --enable-mbstring=shared
         --with-mhash=shared
         --with-mcrypt=shared
         --with-gd=shared
         --with-jpeg-dir=shared
         --with-png-dir=shared
-        --with-tiff-dir=shared
         --with-freetype-dir=shared
+        --disable-pdo
+        --disable-posix
         --enable-gd-native-ttf
         --enable-exif=shared
         --enable-bcmath=shared
@@ -128,12 +126,35 @@ CONFIGURE_OPTS="
         --with-gettext
         --with-iconv
         --enable-sockets
+        --enable-mysqlnd
+        --enable-xml
+        --without-pear
+        --without-iconv
         --with-ldap=shared,/usr/local"
+
+# CFLAGS="$CFLAGS -D_XOPEN_SOURCE=1 -D_XOPEN_SOURCE_EXTENDED=1 -DHAVE_STRINGS_H"
+
+# https://lucamerello.wordpress.com/2015/01/29/solaris-10-how-to-build-and-install-php/
+# https://bugs.php.net/bug.php?id=66013
+create_configure() {
+  logmsg "Create configure file in $TMPDIR/$BUILDDIR"
+  pushd $TMPDIR/$BUILDDIR >/dev/null
+  logcmd /usr/bin/aclocal
+  logcmd /usr/bin/libtoolize
+  logcmd /usr/bin/autoreconf -vi
+  popd >/dev/null
+}
 
 init
 download_source $PROG $PROG $VER
 patch_source
 prep_build
+# create_configure
+
+CFLAGS="-D__EXTENSIONS__ -D_XOPEN_SOURCE=500 -D_XOPEN_SOURCE_EXTENDED=1 -D_POSIX_PTHREAD_SEMANTICS -DHAVE_STRINGS_H"
+#CFLAGS="-D_XOPEN_SOURCE=500 -DHAVE_STRINGS_H"
+# CFLAGS="-D__EXTENSIONS__ -D_XOPEN_SOURCE=500 -D_XOPEN_SOURCE_EXTENDED=1"
+
 build
 
 # Vim hints
---
+++ build/php-7/patches/zend.h
@@ -0,0 +1,19 @@
+--- /Users/jerry/Desktop/php-7.0.13/Zend/zend.h.orig	2016-11-08 16:07:38.000000000 +0100
++++ /Users/jerry/Desktop/php-7.0.13/Zend/zend.h	2016-11-17 21:57:10.000000000 +0100
+@@ -28,6 +28,16 @@
+ 
+ #define ZEND_MAX_RESERVED_RESOURCES	4
+ 
++typedef unsigned char   u_char;
++typedef unsigned short  u_short;
++typedef unsigned int    u_int;
++typedef unsigned long   u_long;
++
++#define INT64_MIN       (-9223372036854775807L-1)
++#elif defined(_LONGLONG_TYPE)
++#define INT64_MIN       (-9223372036854775807LL-1)
++#endif
++
+ #include "zend_types.h"
+ #include "zend_errors.h"
+ #include "zend_alloc.h"
