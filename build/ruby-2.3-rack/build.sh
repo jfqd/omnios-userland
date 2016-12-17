12862560b7d14237a38b8868bd0d9e23ff4b72af
jfqd <jfqd@blun.org>
2016-12-17 17:28:40 +0100

    add rack for ruby-2.3

---
+++ build/ruby-2.3-rack/build.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+# Set Ruby version
+RUBY_VER=2.3
+# Load support functions
+. ../../lib/functions.sh
+. ../../lib/gem-functions.sh
+
+PROG=rack
+VER=1.6.5
+VERHUMAN=$VER
+PKG=runtime/ruby-2.3/rack
+SUMMARY="Gem install of the rack gem"
+DESC="$SUMMARY"
+
+GEM_DEPENDS=""
+
+init
+download_source
+patch_source
+prep_build
+build
+make_isa_stub
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
