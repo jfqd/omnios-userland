7e7ac116f0a34b162ee1e0abf360a3b9c4d92a0d
jfqd <jfqd@blun.org>
2017-03-20 10:15:34 +0100

    downgrade imagemagick to 6.9.8-2

--- build/imagemagick/build.sh
+++ build/imagemagick/build.sh
@@ -28,7 +28,7 @@
 . ../../lib/functions.sh
 
 PROG=ImageMagick
-VER=6.9.8-3
+VER=6.9.8-2
 VERHUMAN=$VER
 PKG=image/imagemagick
 SUMMARY="software suite to create, edit, compose, or convert bitmap images"
