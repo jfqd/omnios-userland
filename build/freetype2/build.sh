39fbe490060b437ea3024de6dfb8d66c58f0d54c
jfqd <jfqd@blun.org>
2017-05-22 16:56:23 +0200

    update freetype to 2.8

--- build/freetype2/build.sh
+++ build/freetype2/build.sh
@@ -28,11 +28,11 @@
 # Load support functions
 . ../../lib/functions.sh
 
-# https://downloads.sourceforge.net/project/freetype/freetype2/2.7.1/freetype-2.7.1.tar.bz2
-# https://downloads.sourceforge.net/project/freetype/freetype2/2.7.1/freetype-2.7.1.tar.bz2.sig
+# https://downloads.sourceforge.net/project/freetype/freetype2/2.8/freetype-2.8.tar.bz2
+# https://downloads.sourceforge.net/project/freetype/freetype2/2.8/freetype-2.8.tar.bz2.sig
 # https://git.savannah.gnu.org/cgit/freetype/freetype2.git/snapshot/freetype2-master.tar.gz
 PROG=freetype
-VER=2.7.1.1
+VER=2.8
 VERHUMAN=$VER
 PKG=library/freetype2
 SUMMARY="A Free, High-Quality, and Portable Font Engine"
