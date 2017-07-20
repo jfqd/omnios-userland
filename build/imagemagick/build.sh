af62f5f873fe92a9aa15b4917a2f872750341472
jfqd <jfqd@blun.org>
2017-07-20 12:39:45 +0200

    update imagemagick to 6.9.9-0

--- build/imagemagick/build.sh
+++ build/imagemagick/build.sh
@@ -28,7 +28,7 @@
 . ../../lib/functions.sh
 
 PROG=ImageMagick
-VER=6.9.8-6
+VER=6.9.9-0
 VERHUMAN=$VER
 PKG=image/imagemagick
 SUMMARY="software suite to create, edit, compose, or convert bitmap images"
